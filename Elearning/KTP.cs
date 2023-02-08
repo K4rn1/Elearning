b781680b05705b44c05ae502bd6d1351fa43aec8
K4rn1 <119027951+K4rn1@users.noreply.github.com>
2023-02-08 23:05:59 +0300

    Update KTP.cs

--- Elearning/KTP.cs
+++ Elearning/KTP.cs
@@ -1,9 +1,18 @@
 using Microsoft.VisualBasic;
 using System;
 
-public class KTP
-{
-    public KTP()
+public class KTP {
+    private Paragraph paragraph;
+    private LessonType lessonType;
+    private List<Content> listcontent;
+    private int nomer;
+    private int chas;
+    public KTP(Paragraph paragraph, LessonType lessonType, List<Content> listcontent, int nomer, int chas)
     {
+        this.paragraph = paragraph;
+        this.lessonType = lessonType;
+        this.listcontent = listcontent;
+        this.nomer = nomer;
+        this.chas = chas;
     }
 }
